6fba555b78088a4b8b72d7357b23f39b2655b576
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-05 10:49:09 +0100

    added angular-fontawesome and importet the basic icon libraries in the main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-root',
@@ -10,4 +13,18 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
     title = 'Connect-U-Frontend';
+
+    constructor(library: FaIconLibrary) {
+        this.fontawesomeSetup(library);
+    }
+
+    /**
+     * brand Icons can be added with var: "fab".
+     * For this project the [icon-library approach](https://github.com/FortAwesome/angular-fontawesome/blob/50cba302bbda97efd6cab68dd8ffe76c806f62c0/docs/usage/icon-library.md) is used.
+     * @param library
+     * @private
+     */
+    private fontawesomeSetup(library: FaIconLibrary): void {
+        library.addIconPacks(fas, far);
+    }
 }
