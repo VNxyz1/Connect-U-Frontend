497ce6e2a8c49e3b3e7ce68b0635521ec0e57256
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-05 12:05:45 +0100

    removed fontawesome from main component, installed primeicons and remixicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {FaIconLibrary} from '@fortawesome/angular-fontawesome';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {far} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-root',
@@ -14,17 +11,7 @@ import {far} from '@fortawesome/free-regular-svg-icons';
 export class AppComponent {
     title = 'Connect-U-Frontend';
 
-    constructor(library: FaIconLibrary) {
-        this.fontawesomeSetup(library);
+    constructor() {
     }
 
-    /**
-     * brand Icons can be added with var: "fab".
-     * For this project the [icon-library approach](https://github.com/FortAwesome/angular-fontawesome/blob/50cba302bbda97efd6cab68dd8ffe76c806f62c0/docs/usage/icon-library.md) is used.
-     * @param library
-     * @private
-     */
-    private fontawesomeSetup(library: FaIconLibrary): void {
-        library.addIconPacks(fas, far);
-    }
 }
