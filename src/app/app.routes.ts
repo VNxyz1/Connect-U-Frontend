16e78833b9e299afcd64c7553ac8a6c4a697dc92
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-06 11:52:36 +0100

    IP2WS2425G1-405
     Basic routing is finished
    
     #IP2WS2425G1-405 comment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import {EventDetailPageComponent} from './views/event-detail-page/event-detail-p
 import {NotFoundPageComponent} from './views/not-found-page/not-found-page.component';
 import {ProfilePageComponent} from './views/profile-page/profile-page.component';
 import {LandingPageComponent} from './views/landing-page/landing-page.component';
+import {isLoggedInGuard} from './utils/guards/is-logged-in.guard';
 
 /**
  * If the user is not logged in, he should be redirected to the landingpage (welcome)
@@ -17,9 +18,9 @@ export const routes: Routes = [
     { path: '', title: 'Home | Connect-U', component: HomePageComponent },
     { path: 'welcome', title: 'Welcome | Connect-U', component: LandingPageComponent },
     { path: 'search', title: 'Search | Connect-U', component: SearchPageComponent },
-    { path: 'create-event', title: 'New Event | Connect-U', component: CreateEventPageComponent },
-    { path: 'my-events', title: 'My Events | Connect-U', component: MyEventsPageComponent },
-    { path: 'my-space', title: 'My Space | Connect-U', component: MySpacePageComponent },
+    { path: 'create-event', title: 'New Event | Connect-U', component: CreateEventPageComponent, canActivate: [isLoggedInGuard] },
+    { path: 'my-events', title: 'My Events | Connect-U', component: MyEventsPageComponent, canActivate: [isLoggedInGuard] },
+    { path: 'my-space', title: 'My Space | Connect-U', component: MySpacePageComponent, canActivate: [isLoggedInGuard] },
     { path: 'event/:id', title: 'Event | Connect-U', component: EventDetailPageComponent },
     { path: 'profile/:id', title: 'Profile | Connect-U', component: ProfilePageComponent },
 
