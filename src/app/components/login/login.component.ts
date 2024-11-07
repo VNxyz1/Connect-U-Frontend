223f20e971a23d3691a65b74f5ce406a5c086737
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-07 21:12:45 +0100

    loginform visual finished, and screanreader compatible

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {PasswordModule} from 'primeng/password';
+import {RouterLink} from "@angular/router";
 
 type LoginForm =  FormGroup<{   email: FormControl<string | null>,  password: FormControl<string | null> }>;
 
@@ -9,21 +10,23 @@ type LoginForm =  FormGroup<{   email: FormControl<string | null>,  password: Fo
 @Component({
   selector: 'app-login',
   standalone: true,
-    imports: [
-        InputTextModule,
-        ReactiveFormsModule,
-        PasswordModule
-    ],
+  imports: [
+    InputTextModule,
+    ReactiveFormsModule,
+    PasswordModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
     form: LoginForm = new FormGroup({
         email: new FormControl<string>('', [Validators.required, Validators.email]),
-        password: new FormControl<string>('', Validators.required),
+        password: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
     })
 
     submitLogin() {
         console.log(this.form.value);
     }
+
 
 }
