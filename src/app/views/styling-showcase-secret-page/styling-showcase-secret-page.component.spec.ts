2cbe1540a57e2452e909c549987cac0efd362840
sirteddi <annalena@as-schaefer.de>
2024-11-10 22:29:27 +0100

    'fix: mergies'

--- src/app/views/styling-showcase-secret-page/styling-showcase-secret-page.component.spec.ts
+++ src/app/views/styling-showcase-secret-page/styling-showcase-secret-page.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StylingShowcaseSecretPageComponent } from './styling-showcase-secret-page.component';
+import { MessageService } from 'primeng/api';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('LandingPageComponent', () => {
+describe('StylingShowcaseSecretPageComponent', () => {
   let component: StylingShowcaseSecretPageComponent;
   let fixture: ComponentFixture<StylingShowcaseSecretPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [StylingShowcaseSecretPageComponent],
+      providers: [
+        MessageService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}), // Mock any route params if needed
+            snapshot: { paramMap: new Map() }, // Mock any snapshot data if needed
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StylingShowcaseSecretPageComponent);
