77d4677b421d2d630c9e8e7c392a1cbd69ca4eca
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-12 18:10:09 +0100

    fix: removed env baseUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:4200',
   apiConfig: {
     urlPrefix: 'http://localhost:3000/api/',
     withCredentials: true,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:4200',
   apiConfig: {
     urlPrefix: '/api/',
     withCredentials: false,
