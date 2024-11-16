fd880e439cd9707873806972999ece51cb11f35e
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-16 11:25:17 +0100

    feature: created interfaces

---
+++ src/app/interfaces/EventCardItem.ts
@@ -0,0 +1,27 @@
+import {EventtypeEnum} from './EventtypeEnum';
+import {Category} from './Category';
+import {StatusEnum} from './StatusEnum';
+
+export interface EventCardItem {
+  id: string;
+
+  categories: Category[];
+
+  dateAndTime: string;
+
+  title: string;
+
+  picture: string;
+
+  status: StatusEnum;
+
+  type: EventtypeEnum;
+
+  isOnline: boolean;
+
+  city?: string;
+
+  participantsNumber: number;
+
+  maxParticipantsNumber: number;
+}
