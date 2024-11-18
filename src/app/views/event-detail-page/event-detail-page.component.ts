7d7eb57df3b1363bad0e2588d97309c5ad5450cb
tinahsr <tinahaser@gmail.com>
2024-11-18 23:01:16 +0100

    feat: started designing detail page

--- src/app/views/event-detail-page/event-detail-page.component.ts
+++ src/app/views/event-detail-page/event-detail-page.component.ts
@@ -1,9 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ImageModule } from 'primeng/image';
+import { TranslocoPipe } from '@jsverse/transloco';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { Button } from 'primeng/button';
+import { AngularRemixIconComponent } from 'angular-remix-icon';
 
 @Component({
   selector: 'app-event-detail-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ImageModule,
+    TranslocoPipe,
+    CardModule,
+    TagModule,
+    Button,
+    AngularRemixIconComponent,
+  ],
   templateUrl: './event-detail-page.component.html',
 })
 export class EventDetailPageComponent {
