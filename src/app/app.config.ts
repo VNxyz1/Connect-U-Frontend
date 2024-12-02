5db85032725e3673a5599b493c0318097f523e32
VNxyz1 <vincenzo_n@rocketmail.com>
2024-12-02 16:32:00 +0100

    changed lang to de

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -95,7 +95,7 @@ const icons = {
 export const translocoConfig = {
   config: {
     availableLangs: ['en-US', 'de'],
-    defaultLang: 'en-US',
+    defaultLang: 'de',
     reRenderOnLangChange: true,
     prodMode: !isDevMode(),
   },
