2f7d1dabc5a4da1b12c5f9bfbab50bf46552dd20
sirteddi <annalena@as-schaefer.de>
2024-12-11 21:46:46 +0100

    mergies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -109,8 +109,8 @@ export const routes: Routes = [
         path: 'guests',
         component: EventGuestsComponent,
         canActivate: [isLoggedInGuard],
-      }
-      ]
+      },
+    ],
   },
   {
     path: 'profile/:id',
