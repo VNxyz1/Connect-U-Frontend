b0634125b28b39c83629faae62d8355a3ed9cfcd
VNxyz1 <vincenzo_n@rocketmail.com>
2024-12-18 19:09:14 +0100

    cicd fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.connectu.app',
   appName: 'Connect-U',
-  webDir: 'dist-app/connect-u-frontend/browser'
+  webDir: 'dist-app/connect-u-frontend/browser',
+  // android: {
+  //   buildOptions: {
+  //     releaseType: 'APK',
+  //     keystorePath: '/home/username/CustomFolder/myCustomName.jks',
+  //     keystorePassword: 'verysecret',
+  //     keystoreAlias: 'custom_alias',
+  //     keystoreAliasPassword: 'alsosecret'
+  //   }
+  // }
 };
 
 export default config;
