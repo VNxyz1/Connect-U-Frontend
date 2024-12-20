6e12dded28b16c256ab344364863cd072890346e
VNxyz1 <vincenzo_n@rocketmail.com>
2024-12-20 15:51:33 +0100

    requested changes from @MrVioz applied. (zumindest di die ich sinnvoll fand)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,9 @@ import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 import { ConfirmationService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { EventService } from '../../services/event/eventservice';
+import { AuthService } from '../../services/auth/auth.service';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -19,22 +22,26 @@ import { EventService } from '../../services/event/eventservice';
     TranslocoPipe,
     ConfirmDialogModule,
     RouterLink,
+    AsyncPipe,
   ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
   currentUrl: string | undefined;
 
+  isLoggedIn!: Observable<boolean>;
+
   constructor(
     private router: Router,
     private confirmationService: ConfirmationService,
     protected translocoService: TranslocoService,
     private eventService: EventService,
+    private authService: AuthService,
   ) {}
 
   ngOnInit() {
     this.currentUrl = this.router.url;
-
+    this.isLoggedIn = this.authService.isLoggedIn();
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
