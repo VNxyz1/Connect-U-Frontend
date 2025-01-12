e3c3eacb5263ce71eee22469a66eacd3a80c6489
VNxyz1 <vincenzo_n@rocketmail.com>
2025-01-12 23:44:13 +0100

    added server availability check

--- src/app/services/push-notification/push-notification.service.ts
+++ src/app/services/push-notification/push-notification.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
-import {SwPush} from '@angular/service-worker';
 
 @Injectable({
   providedIn: 'root',
@@ -11,24 +10,7 @@ export class PushNotificationService {
   private myEventsGuestSubject: BehaviorSubject<number> = new BehaviorSubject<number>(4);
   private myEventsHostSubject: BehaviorSubject<number> = new BehaviorSubject<number>(1);
 
-  private pushSubscription!: PushSubscription;
-
-  constructor(private swPush: SwPush) {
-    swPush.subscription.subscribe({
-      next: subscription => {
-        const isSubscribed = !(subscription === null);
-
-        if (isSubscribed) {
-          console.log('User IS subscribed.');
-        } else {
-          console.log('User is NOT subscribed.');
-        }
-      }
-    })
-  }
-
-  private async initSubscription(swPush: SwPush) {
-    this.pushSubscription = await swPush.requestSubscription({serverPublicKey: "BK0O31ty9muHFIAaGNOjCwzv85uIlU3xai9hM3YA_0LRkaGhlk0LEfpOHV7xnhTLTZXrXUj2ZiVywjWkfkBu89w"});
+  constructor() {
   }
 
   getNavbarMyEvents() {
