df22b42c1b3488c897726cb369d3169e85d5eb77
asc <annalena@as-schaefer.de>
2025-01-14 11:11:39 +0100

    fix header height, fix 404 page

--- src/app/views/not-found-page/not-found-page.component.ts
+++ src/app/views/not-found-page/not-found-page.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { AngularRemixIconComponent } from 'angular-remix-icon';
+import { Button } from 'primeng/button';
+import { PrimeTemplate } from 'primeng/api';
+import { TranslocoPipe } from '@jsverse/transloco';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [
+    AngularRemixIconComponent,
+    Button,
+    PrimeTemplate,
+    TranslocoPipe,
+    RouterLink,
+  ],
   templateUrl: './not-found-page.component.html',
 })
 export class NotFoundPageComponent {}
