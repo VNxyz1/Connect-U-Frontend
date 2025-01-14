a7ff64e85f5f7573b08f37e071edcab3e43c76b1
VNxyz1 <vincenzo_n@rocketmail.com>
2025-01-14 20:40:16 +0100

    get unread message count

--- src/app/services/push-notification/push-notification.service.ts
+++ src/app/services/push-notification/push-notification.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
+import { SocketService } from '../socket/socket.service';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PushNotificationService {
+  private navbarMyEventsSubject: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+  private myEventsGuestSubject: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+  private myEventsHostSubject: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+  private hostedEventsListSubject: BehaviorSubject<Map<string, number>> =
+    new BehaviorSubject<Map<string, number>>(new Map<string, number>());
 
-  private navbarMyEventsSubject: BehaviorSubject<number> = new BehaviorSubject<number>(5);
-  private myEventsGuestSubject: BehaviorSubject<number> = new BehaviorSubject<number>(4);
-  private myEventsHostSubject: BehaviorSubject<number> = new BehaviorSubject<number>(1);
-
-  constructor() {
+  constructor(
+    private socketService: SocketService,
+    private http: HttpClient,
+  ) {
+    this.loadPushNotifications();
   }
 
   getNavbarMyEvents() {
@@ -25,4 +36,43 @@ export class PushNotificationService {
     return this.myEventsHostSubject.asObservable();
   }
 
+  getHostedEventsList() {
+    return this.hostedEventsListSubject.asObservable();
+  }
+
+  private connectHostedEventsSocket() {
+    return this.socketService.on('pushNotificationEvent').pipe(
+      map((events: { eventId: string; notificationCount: number }[]) => {
+        const currentMap: Map<string, number> =
+          this.hostedEventsListSubject.getValue();
+        events.forEach(e => {
+          const existingCount = currentMap.get(e.eventId) || 0;
+          currentMap.set(e.eventId, existingCount + e.notificationCount);
+        });
+        return currentMap;
+      }),
+    );
+  }
+
+  private loadPushNotifications() {
+    this.http
+      .get('push-notification')
+      .pipe(
+        map((data: any) => {
+          const a = new Map<string, number>();
+          for (const e in data) {
+            a.set(e, data[e]);
+          }
+          return a;
+        }),
+        catchError(() => {
+          return of(new Map<string, number>());
+        }),
+      )
+      .subscribe({
+        next: data => {
+          this.hostedEventsListSubject.next(data);
+        },
+      });
+  }
 }
