8db7628ad096d3d986f0b1500b2289b28a1e6d71
Jan Weis <jan-luca.weis@mni.thm.de>
2025-01-15 16:08:58 +0100

    styling + fetching

--- src/app/views/my-friends/my-friends.component.ts
+++ src/app/views/my-friends/my-friends.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProfileCardComponent} from '../../components/profile-card/profile-card.component';
+import {Observable} from 'rxjs';
+import {ProfileData} from '../../interfaces/ProfileData';
+import {FriendsService} from '../../services/friends/friends.service';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-my-friends',
   standalone: true,
-  imports: [],
+  imports: [
+    ProfileCardComponent,
+    AsyncPipe
+  ],
   templateUrl: './my-friends.component.html'
 })
-export class MyFriendsComponent {
+export class MyFriendsComponent implements OnInit {
+  friends$!:Observable<ProfileData[]>;
+  constructor(private friendsService: FriendsService) {}
+
+  ngOnInit(){
+    this.friends$ = this.friendsService.getFriends();
+
+    // Debug: Inhalte in der Konsole anzeigen
+    this.friends$.subscribe({
+      next: (data) => console.log('Fetched Friends:', data),
+      error: (err) => console.error('Error fetching friends:', err)
+    });
+  }
 
 }
