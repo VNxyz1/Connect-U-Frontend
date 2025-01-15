1e57009bdd18a5565966a861393a56f6bcb0882f
Jan Weis <jan-luca.weis@mni.thm.de>
2025-01-15 15:11:42 +0100

    routing + translation + friends comp.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,6 +33,7 @@ import { AddFriendComponent } from './views/add-friend/add-friend.component';
 import { QrCodeAndLinkComponent } from './components/qr-code-and-link/qr-code-and-link.component';
 import { CameraComponent } from './components/camera/camera.component';
 import { ServerUnavailablePageComponent } from './views/server-unavailable-page/server-unavailable-page.component';
+import {MyFriendsComponent} from './views/my-friends/my-friends.component';
 
 /**
  * If the user is not logged in, he should be redirected to the landingpage (welcome)
@@ -165,6 +166,11 @@ export const routes: Routes = [
     path: 'settings',
     title: 'Settings | Connect-U',
     component: SettingsPageComponent,
+  },
+  {
+    path: 'my-friends',
+    title: 'My Friends | Connect-U',
+    component: MyFriendsComponent,
   },
   {
     path: 'information',
---
+++ src/app/views/my-friends/my-friends.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-friends',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-friends.component.html'
+})
+export class MyFriendsComponent {
+
+}
