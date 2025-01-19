9de37466795c68fedef58a26ba3746f385f5dc76
Jan Weis <jan-luca.weis@mni.thm.de>
2025-01-19 11:46:23 +0100

    empty-info

--- src/app/views/my-friends/my-friends.component.ts
+++ src/app/views/my-friends/my-friends.component.ts
@@ -4,11 +4,12 @@ import { Observable } from 'rxjs';
 import { ProfileData } from '../../interfaces/ProfileData';
 import { FriendsService } from '../../services/friends/friends.service';
 import { AsyncPipe } from '@angular/common';
+import {CardModule} from 'primeng/card';
 
 @Component({
   selector: 'app-my-friends',
   standalone: true,
-  imports: [ProfileCardComponent, AsyncPipe],
+  imports: [ProfileCardComponent, AsyncPipe, CardModule],
   templateUrl: './my-friends.component.html',
 })
 export class MyFriendsComponent implements OnInit {
