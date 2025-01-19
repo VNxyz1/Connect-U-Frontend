351b9b4f715a1816249b4031fae56a845bd9b3e8
Jan Weis <jan-luca.weis@mni.thm.de>
2025-01-19 12:30:44 +0100

    format

--- src/app/views/my-friends/my-friends.component.ts
+++ src/app/views/my-friends/my-friends.component.ts
@@ -4,24 +4,27 @@ import { Observable } from 'rxjs';
 import { ProfileData } from '../../interfaces/ProfileData';
 import { FriendsService } from '../../services/friends/friends.service';
 import { AsyncPipe } from '@angular/common';
-import {CardModule} from 'primeng/card';
-import {CarouselModule} from 'primeng/carousel';
-import {EventCardComponent} from '../../components/event-card/event-card.component';
-import {TranslocoPipe} from '@jsverse/transloco';
-
+import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { EventCardComponent } from '../../components/event-card/event-card.component';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-my-friends',
   standalone: true,
-  imports: [ProfileCardComponent, AsyncPipe, CardModule, CarouselModule, EventCardComponent, TranslocoPipe],
+  imports: [
+    ProfileCardComponent,
+    AsyncPipe,
+    CardModule,
+    CarouselModule,
+    TranslocoPipe,
+  ],
   templateUrl: './my-friends.component.html',
 })
 export class MyFriendsComponent implements OnInit {
   friends$!: Observable<ProfileData[]>;
 
-  constructor(
-    private friendsService: FriendsService
-  ) {}
+  constructor(private friendsService: FriendsService) {}
 
   ngOnInit() {
     this.friends$ = this.friendsService.getFriends();
