85bbb1243750510ad08690f2e7fb191f15c4f711
Jan Weis <jan-luca.weis@mni.thm.de>
2025-01-19 12:29:14 +0100

    translated

--- src/app/views/my-friends/my-friends.component.ts
+++ src/app/views/my-friends/my-friends.component.ts
@@ -5,16 +5,23 @@ import { ProfileData } from '../../interfaces/ProfileData';
 import { FriendsService } from '../../services/friends/friends.service';
 import { AsyncPipe } from '@angular/common';
 import {CardModule} from 'primeng/card';
+import {CarouselModule} from 'primeng/carousel';
+import {EventCardComponent} from '../../components/event-card/event-card.component';
+import {TranslocoPipe} from '@jsverse/transloco';
+
 
 @Component({
   selector: 'app-my-friends',
   standalone: true,
-  imports: [ProfileCardComponent, AsyncPipe, CardModule],
+  imports: [ProfileCardComponent, AsyncPipe, CardModule, CarouselModule, EventCardComponent, TranslocoPipe],
   templateUrl: './my-friends.component.html',
 })
 export class MyFriendsComponent implements OnInit {
   friends$!: Observable<ProfileData[]>;
-  constructor(private friendsService: FriendsService) {}
+
+  constructor(
+    private friendsService: FriendsService
+  ) {}
 
   ngOnInit() {
     this.friends$ = this.friendsService.getFriends();
